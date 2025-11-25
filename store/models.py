846de907b8b87307554b04d39234e1e26eae44cc
Rahaf-suq <rahafsuq.442@gmail.com>
2025-11-26 00:01:57 +0300

    Update models.py

--- store/models.py
+++ store/models.py
@@ -6,6 +6,7 @@ class Product(models.Model):
     name = models.CharField(max_length=64)
     price = models.IntegerField()
     description = models.CharField(max_length=200)
+    image_url = models.URLField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.id}: {self.name}"
